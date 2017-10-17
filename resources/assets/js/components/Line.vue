78b4e79bada511288cd8db3acd2e80f13dc1627a
Tony Nguyen <nguyencuonghut55@gmail.com>
2017-10-17 21:18:24 +0700

    Translate labels on growing Client chart into Vietnamese and use the hardcode for values of this chart

--- resources/assets/js/components/Line.vue
+++ resources/assets/js/components/Line.vue
@@ -13,32 +13,32 @@
                 this.Chart = new Chart(this.$refs.canvasline.getContext('2d'), {
                     type: 'line',
                     data: {
-                        labels: ['January', 'February', 'March', 'April', 'May', 'June',
-                                'July', 'August', 'September', 'October', 'November', 'December'],
+                        labels: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6',
+                                'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12'],
                         datasets: [
                             {
                                 label: 'Trại tiềm năng',
                                 borderColor: "red",
-                                data: this.candidate_farms
-                                //data: [40, 39, 10, 40, 39, 20, 40, 30, 10, 20, 50, 90]
+                                //data: this.candidate_farms
+                                data: [40, 39, 10, 40, 39, 20, 40, 30, 10, 20, 50, 90]
                             },
                             {
                                 label: 'Trại mới mở',
                                 borderColor: "green",
-                                data: this.opened_farms
-                                //data: [30, 45, 22, 5, 12, 22, 43, 20, 5, 10, 25, 45]
+                                //data: this.opened_farms
+                                data: [30, 45, 22, 5, 12, 22, 43, 20, 5, 10, 25, 45]
                             },
                             {
                                 label: 'Đại lý tiềm năng',
                                 borderColor: "blue",
-                                data: this.candidate_agents
-                                //data: [30, 29, 5, 20, 19, 30, 30, 40, 20, 4, 30, 60]
+                                //data: this.candidate_agents
+                                data: [30, 29, 5, 20, 19, 30, 30, 40, 20, 4, 30, 60]
                             },
                             {
                                 label: 'Đại lý mới mở',
                                 borderColor: "purple",
-                                data: this.opened_agents
-                                //data: [60, 55, 32, 10, 2, 12, 53, 44, 60, 70, 77, 50]
+                                //data: this.opened_agents
+                                data: [60, 55, 32, 10, 2, 12, 53, 44, 60, 70, 77, 50]
                             },
                         ]
                     },
