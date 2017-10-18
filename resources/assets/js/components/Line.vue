2dc68e57ed6790a21e544a03aed82f98fd5fd390
Tony Nguyen <nguyencuonghut55@gmail.com>
2017-10-18 09:42:54 +0700

    Display the data value on bar chart and line chart

--- resources/assets/js/components/Line.vue
+++ resources/assets/js/components/Line.vue
@@ -44,7 +44,34 @@
                     },
                     options: {
                         responsive: true,
+                        events: false,
+                        tooltips: {
+                            enabled: false
+                        },
+                        hover: {
+                            animationDuration: 0
+                        },
+                        animation:
+                        {
+                            duration: 1,
+                            onComplete: function ()
+                            {
+                                var chartInstance = this.chart,
+                                    ctx = chartInstance.ctx;
+                                ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                                ctx.textAlign = 'center';
+                                ctx.textBaseline = 'bottom';
+                                ctx.fillStyle = 'black';
 
+                                this.data.datasets.forEach(function (dataset, i) {
+                                    var meta = chartInstance.controller.getDatasetMeta(i);
+                                    meta.data.forEach(function (bar, index) {
+                                        var data = dataset.data[index];
+                                        ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                                    });
+                                });
+                            }
+                        }
                     },
                 });
             },
